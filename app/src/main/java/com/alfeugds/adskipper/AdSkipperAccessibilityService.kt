914af71463876d7650b931e5dade622dd2befb59
Victor Herrera <victor.herrera@wizeline.com>
2023-02-02 13:24:49 -0600

    Use bindings in AdSkipperAccessibilityService.kt

--- app/src/main/java/com/alfeugds/adskipper/AdSkipperAccessibilityService.kt
+++ app/src/main/java/com/alfeugds/adskipper/AdSkipperAccessibilityService.kt
@@ -10,16 +10,15 @@ import android.util.Log
 import android.view.accessibility.AccessibilityEvent
 import android.view.accessibility.AccessibilityNodeInfo
 import androidx.preference.PreferenceManager
+import com.alfeugds.adskipper.binding.AccessibilityBinding
 
+
+private const val TAG = "AdSkipperService"
 
 class AdSkipperAccessibilityService : AccessibilityService() {
 
-    private val TAG = "AdSkipperService"
-    private val AD_LEARN_MORE_BUTTON_ID = "com.google.android.youtube:id/player_learn_more_button"
-    private val SKIP_AD_BUTTON_ID = "com.google.android.youtube:id/skip_ad_button"
-    private val AD_PROGRESS_TEXT = "com.google.android.youtube:id/ad_progress_text"
-    private val APP_PROMO_AD_CTA_OVERLAY = "com.google.android.youtube:id/app_promo_ad_cta_overlay"
-    private val AD_COUNTDOWN = "com.google.android.youtube:id/ad_countdown"
+    private var _binding: AccessibilityBinding? = null
+    private val binding: AccessibilityBinding get() = _binding!!
 
     private var isMuted = false
     var isRunning = false
@@ -31,6 +30,7 @@ class AdSkipperAccessibilityService : AccessibilityService() {
         Log.i(TAG, "onCreate fired")
         audioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
         prefs = PreferenceManager.getDefaultSharedPreferences(this)
+        _binding = AccessibilityBinding(this)
     }
 
     override fun onRebind(intent: Intent?) {
@@ -62,6 +62,7 @@ class AdSkipperAccessibilityService : AccessibilityService() {
     override fun onDestroy() {
         Log.v(TAG, "onDestroy fired")
         isRunning = false
+        _binding = null
         super.onDestroy()
     }
 
@@ -139,31 +140,12 @@ class AdSkipperAccessibilityService : AccessibilityService() {
     }
 
     override fun onAccessibilityEvent(event: AccessibilityEvent?) {
-
-        //optional resource ids
-        // com.google.android.youtube:id/ad_progress_text --> ad duration
-
-        // required resource ids:
-        // [visitar anunciante text]
-        // com.google.android.youtube:id/player_learn_more_button --> class android.widget.TextView
-
-        // [skip ad button]
-        // com.google.android.youtube:id/skip_ad_button --> class android.widget.FrameLayout
-
+        if (!isServiceEnabled()) {
+            Log.i(TAG, "Service is not supposed to be enabled.")
+            return
+        }
         try {
-
-            if (!isServiceEnabled()) {
-                Log.i(TAG, "Service is not supposed to be enabled.")
-                return
-            }
-
-            val adLearnMoreElement = rootInActiveWindow?.findAccessibilityNodeInfosByViewId(AD_LEARN_MORE_BUTTON_ID)?.getOrNull(0)
-            val skipAdButton = rootInActiveWindow?.findAccessibilityNodeInfosByViewId(SKIP_AD_BUTTON_ID)?.getOrNull(0)
-            val adProgressText = rootInActiveWindow?.findAccessibilityNodeInfosByViewId(AD_PROGRESS_TEXT)?.getOrNull(0)
-            val appPromoAdCTAOverlay = rootInActiveWindow?.findAccessibilityNodeInfosByViewId(APP_PROMO_AD_CTA_OVERLAY)?.getOrNull(0)
-            val adCountdown = rootInActiveWindow?.findAccessibilityNodeInfosByViewId(AD_COUNTDOWN)?.getOrNull(0)
-
-            if (adLearnMoreElement == null && skipAdButton == null && adProgressText == null && appPromoAdCTAOverlay == null && adCountdown == null) {
+            if (binding.hasNoBindings) {
                 unmuteMedia()
                 Log.v(TAG, "No ads yet...")
                 return
@@ -172,17 +154,13 @@ class AdSkipperAccessibilityService : AccessibilityService() {
 
             muteMedia()
 
-            if (skipAdButton == null) {
-                Log.v(TAG, "skipAdButton is null... returning...")
-                return
-            }
-
-            if (skipAdButton.isClickable) {
-                Log.v(TAG, "skipAdButton is clickable! Trying to click it...")
-                skipAdButton.performAction(AccessibilityNodeInfo.ACTION_CLICK)
-                Log.i(TAG, "Clicked skipAdButton!")
-            }
-
+            binding.skipAdButton
+                ?.takeIf { it.isClickable }
+                ?.let { skipAdButton ->
+                    Log.v(TAG, "skipAdButton is clickable! Trying to click it...")
+                    skipAdButton.performAction(AccessibilityNodeInfo.ACTION_CLICK)
+                    Log.i(TAG, "Clicked skipAdButton!")
+                } ?: Log.v(TAG, "skipAdButton is null... returning...")
         } catch (error: Exception) {
             Log.e(TAG, "Something went wrong...", error)
         }
